01dc8ceb61deb22bd4f3fc84cf7c559ed9292886
Michael Wilkins <mpwilkins95@gmail.com>
2022-03-25 17:00:43 -0700

    Commented on the four sections of equations

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	math
 )
 
+
+/* Main will be used to call each section as necessary ranging from:
+1. Pre-Algebra / Elementary Algebra
+2. Intermediate Algebra / Coordinate Geometry 
+3. Plane Geometry
+4. Trigonometry */
 func main() {
 	
 }
